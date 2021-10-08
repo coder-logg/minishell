b6a029ba9dc9e3f804e314eab509e7f34d490c7d
Telly Phlogiston <tphlogis@ev-f5.kzn.21-school.ru>
2021-10-09 01:00:12 +0300

    made recording to minish.cmd (without pipes), pasing quotes

--- parser/change_string.c
+++ parser/change_string.c
@@ -1,27 +1,60 @@
 #include "../minishell.h"
 
 /**
- * replace substring in string
+ * Replace first occurrence substring <b><i>substr</i></b>
+ * in string <b><i>str</i></b>
  * @param str string to search substring
  * @param substr string which will be replaced if found
  * @param replacement string to replacement
+ * @param pos position in <b><i>str</i></b> from which to search
+ * <b><i>substr</i></b>
  * @return new string in which first found <b><i>substr</i></b> will be
- * replaced with <b><i>replacement</i></b> or <b><i>NULL</i></b> if wasn't found
+ * replaced with <b><i>replacement</i></b> or <b><i>str</i></b> if wasn't found
  */
-char	*replace_subst(char *str, char *substr, char *replacement)
+char	*replace_subst(char *str, char *substr, char *replacement, int pos)
 {
 	char	*res;
 	char	*place;
 	int		len;
 
-	len = ft_strlen(str) - ft_strlen(substr) + ft_strlen(replacement);
-	res = ft_calloc(len + 1, sizeof(char));
-	place = ft_strstr(str, substr);
+	place = ft_strstr(str + pos, substr);
 	if (place == NULL)
-		return (NULL);
+		return (str);
+	len = ft_strlen(str) - ft_strlen(substr) + ft_strlen(replacement);
+	res = check_malloc(ft_calloc(len + 1, sizeof(char)));
 	len = place - str;
 	ft_strlcpy(res, str, len + 1);
 	ft_strcpy(res + len, replacement);
 	ft_strcpy(res + len + ft_strlen(replacement), place + ft_strlen(substr));
+	return (res);
+}
+
+/**
+ * Collapse few repeating characters to a single character.
+ * @param str string to change
+ * @param pos position with symbols to replace
+ * @param ch symbol to collapse
+ * @return string without repeating characters <b><i>ch</i></b>
+ * in this position in string or <b><i>str</i></b> if there isn't
+ * repeating characters
+ */
+char	*collapse_symbols(char *str, int pos, char ch)
+{
+	int		i;
+	char	*to_collapse;
+	char	*res;
+	char	ch_str[2];
+
+	i = 0;
+	ch_str[0] = ch;
+	ch_str[1] = '\0';
+	while (str[pos + i] == ch)
+		i++;
+	if (i == 0 || i == 1)
+		return (str);
+	to_collapse = ft_calloc(i + 1, sizeof(char));
+	ft_strlcpy(to_collapse, str + pos, i + 1);
+	res = replace_subst(str, ch_str, to_collapse, pos);
+	free(to_collapse);
 	return (res);
 }
