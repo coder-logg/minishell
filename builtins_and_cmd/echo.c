12f444cbaea6d361d34d4e7505f4dcc1ce51337d
Carlos Venkman <cvenkman@si-f3.kzn.21-school.ru>
2021-10-25 16:49:53 +0300

    fix echo

--- builtins_and_cmd/echo.c
+++ builtins_and_cmd/echo.c
@@ -6,7 +6,7 @@
 /*   By: cvenkman <cvenkman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 19:29:09 by cvenkman          #+#    #+#             */
-/*   Updated: 2021/10/24 20:58:28 by cvenkman         ###   ########.fr       */
+/*   Updated: 2021/10/25 16:48:38 by cvenkman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,28 @@
 
 int echo(char **cmd_splited)
 {
-	int	i;
+	int		i;
+	int		len;
+	_Bool	flag;
 
 	i = 1;
-	if (!ft_strcmp(cmd_splited[1], "-n"))
+	len = 0;
+	flag = false;
+	while (cmd_splited[len])
+		len++;
+	if (len != 1 && !ft_strcmp(cmd_splited[1], "-n"))
+	{
 		i = 2;
-	if (cmd_splited[1])
+		flag = true;
+	}
+	if (len > 1)
 		while (cmd_splited[i])
 		{
 			ft_putstr_fd(cmd_splited[i++], 1);
 			if (cmd_splited[i] != NULL)
 				write (1, " ", 1);
 		}
-	if (ft_strcmp(cmd_splited[1], "-n") != 0)
+	if (flag == false)
 		write (1, "\n", 1);
 	return (0);
 }
